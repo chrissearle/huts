8a715c6d4e2af0bb2afc4c88bd8d1e651d91b33e
Chris Searle <chris@chrissearle.org>
2008-03-05 08:56:06 +0100

    [svn] o Refs #29 o Bootstrap user is now admin/admin

--- trunk/grails-app/conf/BootStrap.groovy
+++ trunk/grails-app/conf/BootStrap.groovy
@@ -1,9 +1,9 @@
 class BootStrap {
 
     def init = {servletContext ->
-        if (!Person.findByUserId("chris")) {
-            new Person(name: "Chris Searle", userId: "chris", password: "cdsamw",
-                    phone: "932 55 054", email: "chris@chrissearle.org", admin: true, approved: true).save()
+        if (!Person.get(1)) {
+            new Person(name: "Default Admin - Change these details to your own!", userId: "admin", password: "admin",
+                    phone: "xxxxxxxx", email: "root@localhost", admin: true, approved: true).save()
         }
     }
     def destroy = {
