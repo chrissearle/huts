d2d3ddbf367ff8274684639b1c311a4c936fbffa
Chris Searle <chris@chrissearle.org>
2008-03-11 15:56:42 +0100

    [svn] o Remove doubled i18n file

--- trunk/grails-app/conf/BootStrap.groovy
+++ trunk/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,8 @@
 class BootStrap {
 
     def init = {servletContext ->
+        Locale.setDefault(new Locale("nb"))
+
         if (!Person.get(1)) {
             new Person(name: "Default Admin - Change these details to your own!", userId: "admin", password: "admin",
                     phone: "xxxxxxxx", email: "root@localhost", admin: true, approved: true).save()
